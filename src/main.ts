9b995a35226d681f008330881a665d5c99b5f7d7
王伟 <wangwei@jlkj.net>
2024-05-29 17:16:59 +0800

    feat：删除eslint的规则。有问题，需要重新配置，执行打包example以及不同的模式版本配置

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,19 @@
-import typescriptSVG from './typescript.svg';
 export default class HelloLib {
-
   public container: HTMLElement | null;
   constructor(container: string | HTMLElement) {
-    console.log('HelloLib created,HELLOWorld');
-    this.container = typeof container === 'string' ? document.getElementById(container) : container;
+    console.log("HelloLib created,HELLOWorld");
+    this.container =
+      typeof container === "string"
+        ? document.getElementById(container)
+        : container;
     if (!this.container) {
-      console.error('container not found');
-      return
+      console.error("container not found");
+      return;
     }
-
-
-
-
-
-
-    this.container.innerHTML = `
-        <H1>Hello my lib</H1>
-        <div class="logo">
-        <img src="${typescriptSVG}" alt="typescript logo" class="svg" />
-        <img src="/vite.svg" alt="vite" class="svg" />
-        </div>
-    `
-
+    this.container.innerHTML = "Hello from lib";
   }
 
   public static sayHello(): string {
-    return 'Hello from lib';
+    return "Hello from lib";
   }
 }
